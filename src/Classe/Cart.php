b7cfff108f93c1fd538f85fbb7fec68b41520c06
YannickCourbic15987 <a.yacine2020@laposte.net>
2022-07-20 22:29:12 +0200

    maj  pannier

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,24 +2,58 @@
 
 namespace App\Classe;
 
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+// use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart
 {
+    private $stack;
 
+    public function __construct(RequestStack $stack)
 
-
-    public function __construct(SessionInterface $session)
     {
-        $this->session = $session;
+        return $this->stack = $stack;
     }
-    public function add($id)
+
+    public function addTo($id)
     {
-        $this->session->set('cart', [
+
+        $session = $this->stack->getSession();
+        $cart = $session->get('cart', [
+
             [
-                '$id' => $id,
+                'id' => $id,
                 'quantity' => 1
             ]
         ]);
+
+        // if (!empty($cart[$id])) {
+        //     $cart[$id]++;
+        // } else {
+        //     $cart[$id] = 1;
+        // }
+
+
+        $session->set('cart', $cart);
     }
+
+    public function getTo()
+    {
+        $session_get = $this->stack->getSession();
+        return dd($session_get->get('cart'));
+    }
+
+
+    public function removeTo()
+    {
+        $session_remove = $this->stack->getSession();
+        return $session_remove->remove('cart');
+    }
+
+    // public function remove()
+    // {
+
+    //     $methodremove = $this->stack->getSession();
+    //     return $methodremove->remove('cart');
+    // }
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use App\Classe\Cart;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,15 +11,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CartController extends AbstractController
 {
     #[Route('/mon-panier', name: 'cart')]
-    public function index(): Response
+    public function index(Cart $cart): Response
     {
+        $cart->getTo();
         return $this->render('cart/index.html.twig');
     }
     #[Route('/cart/add/{id}', name: 'add_to_cart')]
     public function add(Cart $cart, $id): Response
     {
-        $cart->add($id);
+        $cart->addTo($id);
 
-        return $this->render('cart/index.html.twig');
+        return $this->redirectToRoute('cart');
+    }
+    #[Route('/cart/remove', name: 'remove_to_cart')]
+    public function removeTo(Cart $cart): Response
+    {
+        $cart->removeTo();
+
+        return $this->redirectToRoute('products');
     }
 }
