1a537d4691e5bd99c911ce7e598c726ed1c7ff0a
YannickCourbic15987 <a.yacine2020@laposte.net>
2022-07-21 22:19:04 +0200

    ajout de l'addresse

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
+use App\Form\AddressType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountAddressController extends AbstractController
 {
-    #[Route('/account/address', name: 'account_address')]
+    #[Route('/compte/adresses', name: 'account_address')]
     public function index(): Response
     {
+
+
+        // dd($this->getUser());
         return $this->render('account/address.html.twig');
+    }
+    #[Route('/compte/ajouter-une-adresse', name: 'account_address_add')]
+    public function addAddress(): Response
+    {
+        $address = new Address();
+
+        $form = $this->createForm(AddressType::class, $address);
+
+
+        // dd($this->getUser());
+        return $this->render('account/address_add.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/AddressType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Quel nom souhaitez-vous donner à votre adresse ?',
+                'attr' => [
+                    'placeholder' => 'nommez votre adresse '
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre Prénom',
+                'attr' => [
+                    'placeholder' => 'entrez votre prénom '
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre Nom',
+                'attr' => [
+                    'placeholder' => 'entrez votre nom '
+                ]
+            ])
+            ->add('company', TextType::class, [
+                'label' => 'Votre entreprise , si oui laquelle ?',
+                'attr' => [
+                    'placeholder' => 'entrez le nom de l\'entreprise (falcutatif)'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Votre adresse ?',
+                'attr' => [
+                    'placeholder' => '8 rue des lilas...'
+                ]
+            ])
+            ->add('postal', TextType::class, [
+                'label' => 'votre Carte Postal ',
+                'attr' => [
+                    'placeholder' => 'entrez votre Carte Postal '
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'ville',
+                'attr' => [
+                    'placeholder' => 'entrez le nom de la ville'
+                ]
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'votre pays ',
+                'attr' => [
+                    'placeholder' => 'le nom du pays'
+                ]
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Votre numéro de télèphone',
+                'attr' => [
+                    'placeholder' => 'numéro de télèphone'
+                ]
+            ])
+            ->add('submit', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
